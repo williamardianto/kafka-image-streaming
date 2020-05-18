2bffe6de4569d8c926d62320a9a7fbd371808501
William Ardianto <william.ardianto@skymind.my>
2020-05-18 15:46:18 +0800

    init commit

---
+++ src/main/java/ImageConsumer.java
@@ -0,0 +1,68 @@
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.ByteArrayDeserializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.bytedeco.javacpp.BytePointer;
+import org.bytedeco.javacv.CanvasFrame;
+import org.bytedeco.javacv.OpenCVFrameConverter;
+import org.bytedeco.opencv.opencv_core.Mat;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
+import static org.opencv.core.CvType.CV_8UC;
+
+public class ImageConsumer {
+    static String KAFKA_BROKERS = "localhost:9093";
+    static Logger log = LoggerFactory.getLogger(ImageConsumer.class);
+
+    static String groupId = "smart-retail";
+    static String topic = "image";
+
+    static int width = 1920 / 2;
+    static int height = 1080 / 2;
+
+    public static void main(String[] args) {
+        Properties properties = new Properties();
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, KAFKA_BROKERS);
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ByteArrayDeserializer.class.getName());
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
+        properties.put(ConsumerConfig.FETCH_MAX_BYTES_CONFIG, "2097152");
+
+        KafkaConsumer<String, byte[]> consumer = new KafkaConsumer<>(properties);
+
+        consumer.subscribe(Collections.singleton(topic));
+
+        CanvasFrame canvas = new CanvasFrame("Consumer Canvas");
+        canvas.setCanvasSize(width, height);
+
+        OpenCVFrameConverter.ToMat converter = new OpenCVFrameConverter.ToMat();
+
+        while (true) {
+            ConsumerRecords<String, byte[]> records = consumer.poll(Duration.ofMillis(1000));
+
+            records.forEach(record -> {
+
+                log.info("Record: \n" +
+                        "Topic:" + record.topic() + "\n" +
+                        "Partition:" + record.partition() + "\n" +
+                        "Offset: " + record.offset() + "\n" +
+                        "Timestamp: " + record.timestamp()
+                );
+
+                Mat mat = BytesToMat(record.value());
+                canvas.showImage(converter.convert(mat));
+            });
+        }
+    }
+
+    static Mat BytesToMat(byte[] b) {
+        return new Mat(height, width, CV_8UC(3), new BytePointer(b));
+    }
+}
---
+++ src/main/java/ImageProducer.java
@@ -0,0 +1,101 @@
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.ByteArraySerializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.bytedeco.javacv.CanvasFrame;
+import org.bytedeco.javacv.FFmpegFrameGrabber;
+import org.bytedeco.javacv.Frame;
+import org.bytedeco.javacv.OpenCVFrameConverter;
+import org.bytedeco.opencv.opencv_core.Mat;
+import org.bytedeco.opencv.opencv_core.Size;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+import static org.bytedeco.opencv.global.opencv_imgproc.resize;
+
+public class ImageProducer {
+    static Logger log = LoggerFactory.getLogger(ImageProducer.class);
+
+    static String KAFKA_BROKERS = "localhost:9093";
+
+    public static void main(String[] args) {
+        Properties properties = new Properties();
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, KAFKA_BROKERS);
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, ByteArraySerializer.class.getName());
+        properties.put(ProducerConfig.ACKS_CONFIG, "all");
+        properties.put(ProducerConfig.MAX_REQUEST_SIZE_CONFIG, "2097152");
+        properties.put(ProducerConfig.COMPRESSION_TYPE_CONFIG, "snappy");
+
+        KafkaProducer<String, byte[]> producer = new KafkaProducer<>(properties);
+
+        OpenCVFrameConverter.ToMat converter = new OpenCVFrameConverter.ToMat();
+
+        String homePath = System.getProperty("user.home");
+        try (FFmpegFrameGrabber frameGrabber = new FFmpegFrameGrabber(homePath + "/Desktop/timer.mp4")) {
+            frameGrabber.start();
+
+            int width = frameGrabber.getImageWidth() / 2;
+            int height = frameGrabber.getImageHeight() / 2;
+            long length = frameGrabber.getLengthInTime();
+
+            Frame frame;
+
+            CanvasFrame canvas = new CanvasFrame("Producer Canvas");
+            canvas.setCanvasSize(width, height);
+
+//            log.info("framerate = " + frameGrabber.getFrameRate());
+//            System.out.println("length = "+ length);
+
+            while (canvas.isVisible() && (frame = frameGrabber.grab()) != null) {
+//                canvas.showImage(frame);
+
+                Mat frameMat = converter.convert(frame); //1920 x 1080
+                resize(frameMat, frameMat, new Size(width, height)); // 480x270
+
+                canvas.showImage(converter.convert(frameMat));
+
+                byte[] frameBytes = MatToBytes(frameMat);
+
+//                Mat newFrame = BytesToMat(frameBytes);
+//                imshow("image",newFrame);
+//                waitKey(0);
+
+                ProducerRecord<String, byte[]> record = new ProducerRecord<>("image", frameBytes);
+                producer.send(record, new ProducerCallback());
+
+//                producer.flush();
+
+                Thread.sleep(100);
+            }
+
+            frameGrabber.stop();
+            canvas.dispose();
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private static byte[] MatToBytes(Mat mat) {
+        byte[] b = new byte[mat.channels() * mat.cols() * mat.rows()];
+        mat.data().get(b);
+        return b;
+    }
+
+    static class ProducerCallback implements Callback {
+
+        @Override
+        public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+            if (e == null) {
+                log.info("Successfully received the details as: \n" +
+                        "Topic:" + recordMetadata.topic() + "\n" +
+                        "Partition:" + recordMetadata.partition() + "\n" +
+                        "Offset: " + recordMetadata.offset() + "\n" +
+                        "Timestamp: " + recordMetadata.timestamp());
+            } else
+                log.error(e.getMessage());
+        }
+    }
+}
